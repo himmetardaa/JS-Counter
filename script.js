586d9c8a68e9ca1cf09887ca4cf6b2ddf2586f3d
himmetarda <himmetardasargin@hotmail.com>
2023-06-01 10:40:23 +0300

    sayac uygulaması yapıldı.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript";
+const artı = document.getElementById("artı");
+const eksi = document.getElementById("eksi");
+const sonuc = document.getElementById("sonuc");
+let sonuc_ınt = parseInt(sonuc.innerHTML);
+
+artı.addEventListener("click", (event) => {
+    sonuc.innerText = ++sonuc_ınt;
+}) // callback function
+
+eksi.addEventListener("click", (event) => {
+
+    const azaltilanDeger = --sonuc_ınt;
+
+    if (azaltilanDeger <= 0) {
+        alert("Minimum değere ulaştınız!");
+        sonuc.innerText = "0";
+        sonuc_ınt = 0;
+        return;
+    }
+    
+    sonuc.innerText = azaltilanDeger;
+})
+
